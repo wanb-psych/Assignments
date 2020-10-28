4d4f442a4e0c9f3fd847a62c39be4f58a98495c2
Bin Wan <wanb.psych@outlook.com>
2020-10-28 21:47:09 +0100

    Create G1-10 loop.py

---
+++ G1-10 loop.py
@@ -0,0 +1,21 @@
+$ import pandas as pd
+$ filename = '/data/hu_binwan/Desktop/covariance_gradients.csv'
+$ dataframe = pd.read_csv(filename, header=None)
+
+$ import numpy as np
+$ from brainspace.datasets import load_group_fc, load_parcellation, load_conte69
+$ from brainspace.utils.parcellation import map_to_labels
+
+$ surf_lh, surf_rh = load_conte69()
+$ labeling = load_parcellation('schaefer', scale=400, join=True)
+$ mask = labeling != 0
+
+$ grad_labelled = [None] * 10
+$ for i in range (10):
+    grad_labelled[i] = map_to_labels(np.array(dataframe[i]), labeling, mask=mask, fill=np.nan)
+    
+$ from brainspace.plotting import plot_hemispheres
+$ plot_hemispheres(surf_lh, surf_rh, array_name=grad_labelled, size=(3600, 800), cmap='viridis_r',
+                 color_bar=True, label_text=['Grad1','Grad2','Grad3','Grad4','Grad5','Grad6','Grad7','Grad8','Grad9','Grad10'], zoom=1.2, embed_nb=True)
+
+
