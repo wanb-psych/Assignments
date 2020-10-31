6db7638bd93826e9f67cef3a15663e4c57074cda
Bin Wan <wanb.psych@outlook.com>
2020-10-31 01:11:21 +0100

    Update G1-10 loop.py

--- G1-10 loop.py
+++ G1-10 loop.py
@@ -15,7 +15,7 @@ $ for i in range (10):
     grad_labelled[i] = map_to_labels(np.array(dataframe[i]), labeling, mask=mask, fill=np.nan)
     
 $ from brainspace.plotting import plot_hemispheres
-$ plot_hemispheres(surf_lh, surf_rh, array_name=grad_labelled, size=(3600, 800), cmap='viridis_r',
-                 color_bar=True, label_text=['Grad1','Grad2','Grad3','Grad4','Grad5','Grad6','Grad7','Grad8','Grad9','Grad10'], zoom=1.2, embed_nb=True)
+$ plot_hemispheres(surf_lh, surf_rh, array_name=grad_labelled, size=(1600, 2800), cmap='viridis_r',
+                 color_bar=True, label_text=['Grad 1','Grad 2','Grad 3','Grad 4','Grad 5','Grad 6','Grad 7','Grad 8','Grad 9','Grad 10'], zoom=1.2, embed_nb=True)
 
 
